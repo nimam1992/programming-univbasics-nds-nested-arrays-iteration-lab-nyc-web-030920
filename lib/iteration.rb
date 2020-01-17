0e8701d1ed356564bafade65c6d5ac39a87c4604
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-17 05:12:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -56,7 +56,7 @@ def total_even_pairs(src)
   while count < src.length do
     count_B = 0
     while count_B < src.length[count]-1 do
-      if src[count][count_B]%2==0 && src[count][src.length[count]-1]%2==0
+      if src[count][count_B]%2==0
         total += src[count][count_B]+src[count][src.length[count]-1]
         total+=sum
       end
